8e72696606c18b7cd0a6f78732a6859d965955ef
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-04-15 00:12:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", "5791145987")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Dipanshu_021:ad8920@cluster0.f7migc1.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
-    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data1')
     SESSION = os.environ.get("SESSION", "BQAL7OVbfMurYfUeMU0GJ6YSImwcBHIeV5l-UwJplbP7Xnp9rQjHOd50ouM0cmNokfyMqzHfm1OLkASGrr_nQTwTIgej3OVQFFNrAvfPeLqOl0eQRbK_q_GLww4OC3NElJYnkjRrV3H7ocK3BA1YG3r-sZPigYfSxZeC_4Mvho9TWRhygXCqu4kY0xVAr5RD71W4e1aPH-kVB9AfO7L54c3oMTfXodsT4WZPx7alyfPZJ7_-29LsAI9zlKgH_5j144BhimqHGwH3DcmifpvOm-JORPjDcIwwvVZGev8d8MDPq0Jweip8vzZW28S5lLKktJqDyktNYhZsJhOE9oNY2EB4AAAAATMiN8EA")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001974954587"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Disjsjs_bot")
